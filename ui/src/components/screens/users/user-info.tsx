692c58cb1dd2a13a72f1af47817d76530b8e6c51
pingzhu <ping.zhu@microfocus.com>
2022-03-26 15:52:29 +0800

    fix sub route bugs

--- ui/src/components/screens/users/user-info.tsx
+++ ui/src/components/screens/users/user-info.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Navigate } from "react-router";
 import { Link, Route, Routes } from "react-router-dom";
 import { Epic } from "./epic";
 import { Kanban } from "./kanban";
@@ -12,6 +13,7 @@ export const UserInfo = () => {
       <Routes>
         <Route path="/kanban" element={<Kanban />} />
         <Route path="/epic" element={<Epic />} />
+        <Route path="*" element={<Navigate to={window.location.pathname + "/kanban"}/>}/>
       </Routes>
     </div>
   );
--- ui/src/routes/index.tsx
+++ ui/src/routes/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Home } from "@src/components/home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { NavigationBar } from "@src/components/home/navigation-bar";
 import { UsersPage } from "@src/components/screens/users";
 import { UserInfo } from "@src/components/screens/users/user-info";
+import { Navigate, Route, Routes } from "react-router";
 
 export const PortalRoute = () => {
   return (
@@ -14,6 +15,7 @@ export const PortalRoute = () => {
           <Route path="/" element={<Home />} />
           <Route path="/users" element={<UsersPage />} />
           <Route path="/users/:id/*" element={<UserInfo />} />
+          <Route path="*" element={<Navigate to={"/"} />}/>
         </Routes>
       </BrowserRouter>
     </>
